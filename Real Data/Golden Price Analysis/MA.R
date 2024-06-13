842597445a49929df23d232baf12d71d1a1b7f23
han-gnat <hedy890808@gmail.com>
2024-06-13 23:13:53 +0800

    Update MA.R

--- Real Data/Golden Price Analysis/MA.R
+++ Real Data/Golden Price Analysis/MA.R
@@ -1,6 +1,7 @@
 library(readr)
 library(ggplot2)
 
+# Load data
 Golden <- read_csv("price.csv")
 price <- Golden$price
 Date <- as.Date(Golden$Date)
@@ -10,10 +11,12 @@ mu <- mean(x[1:337])
 sigma <- sd(x)
 n <- length(x)
 
+# set parameter
 window=3
 L=2.949
 
-RL=wt=vj=c()
+# weight of MA
+RL = wt = vj = c()
 for(i in 1:n){ 
   if(i >= window){
     wt[i] = 1 / window
@@ -31,8 +34,8 @@ for(j in 1:n){
   }
 }
 
-ma=ucl=lcl=c()
-
+# compute statistic of MA, UCL and LCL
+ma = ucl = lcl = c()
 for(j in 1:n){
   if(j < window){
     ma[j] = mean(x[1:j])
@@ -40,12 +43,12 @@ for(j in 1:n){
   else{
     ma[j] = mean(x[(j-window+1):j])
   }
-  ucl[j]=mu+L*sqrt(vj[j])
-  lcl[j]=mu-L*sqrt(vj[j])
+  ucl[j] = mu+L*sqrt(vj[j])
+  lcl[j] = mu-L*sqrt(vj[j])
 }
 
+# Find the first index that exceeds the control limit
 exceed_indices <- which(ma > ucl)[1]
-print(exceed_indices)
 
 ma_df <- data.frame(Date = Date, MA = ma, UCL = ucl, LCL = lcl)
 
@@ -55,8 +58,8 @@ ggplot(ma_df, aes(x = Date)) +
   geom_line(aes(y = LCL), color = "red", linetype = "solid") +
   labs(x = "Date", y = "MA", title = "MA Control Chart") +
   theme_bw() + 
-  theme(panel.grid=element_blank()) +
+  theme(panel.grid = element_blank()) +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_point(data = ma_df[exceed_indices, ], aes(x = Date, y = MA), color = "blue", size = 1.5) +
-  geom_text(data = ma_df[exceed_indices, ], aes(x = Date, y = MA, label = format(Date, "%Y-%m")), vjust = -1.5,hjust=0.95, color = "blue",size=3.3)
+  geom_text(data = ma_df[exceed_indices, ], aes(x = Date, y = MA, label = format(Date, "%Y-%m")), vjust = -1.5, hjust = 0.95, color = "blue", size = 3.3)
 

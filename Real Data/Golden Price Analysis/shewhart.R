c49c553e0c00affdc8e3445902da4a797134c659
han-gnat <hedy890808@gmail.com>
2024-06-13 23:11:41 +0800

    Update shewhart.R

--- Real Data/Golden Price Analysis/shewhart.R
+++ Real Data/Golden Price Analysis/shewhart.R
@@ -1,6 +1,7 @@
 library(readr)
 library(ggplot2)
 
+# Load data
 Golden <- read_csv("price.csv")
 price <- Golden$price
 Date <- as.Date(Golden$Date)
@@ -10,11 +11,13 @@ mu <- mean(x[1:337])
 sigma <- sd(x)
 n <- length(x)
 
+# set parameter
 q <- 0
 alpha <- 1
 L <- 3
 
-wt <- vj <- c()
+# weight of shewhart
+wt = vj = c()
 for(i in 1:n) {
   wt[i] <- q^((i-1)^alpha)-q^(i^alpha)
 }
@@ -22,7 +25,8 @@ for(j in 1:n) {
   vj[j] <- sum(wt[1:j]*wt[1:j])*(sigma^2)
 }
 
-shewhart <- ucl <- lcl <- c()
+# compute statistic of shewhart, UCL and LCL
+shewhart = ucl = lcl = c()
 for(j in 1:n) {
   zsum <- sum(wt[1:j]*x[j:1]) + mu*(q^(j^alpha))
   shewhart[j] <- zsum
@@ -30,19 +34,19 @@ for(j in 1:n) {
   lcl[j] <- mu - L*sqrt(vj[j])
 }
 
+# Find the first index that exceeds the control limit
 exceed_indices <- which(shewhart > ucl)[1]
-print(exceed_indices)
 
 shewhart_df <- data.frame(Date = Date, Shewhart = shewhart, UCL = ucl, LCL = lcl)
 
+# plot result
 ggplot(shewhart_df, aes(x = Date)) +
   geom_line(aes(y = Shewhart), color = "black") +
   geom_line(aes(y = UCL), color = "red", linetype = "solid") +
   geom_line(aes(y = LCL), color = "red", linetype = "solid") +
   labs(x = "Date", y = "Shewhart", title = "Shewhart Control Chart") +
   theme_bw() + 
-  theme(panel.grid=element_blank()) +
+  theme(panel.grid = element_blank()) +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_point(data = shewhart_df[exceed_indices, ], aes(x = Date, y = Shewhart), color = "blue", size = 1.5) +
-  geom_text(data = shewhart_df[exceed_indices, ], aes(x = Date, y = Shewhart, label = format(Date, "%Y-%m")), vjust = -1,hjust=0.4, color = "blue",size=3.3)
-
+  geom_text(data = shewhart_df[exceed_indices, ], aes(x = Date, y = Shewhart, label = format(Date, "%Y-%m")), vjust = -1, hjust = 0.4, color = "blue", size = 3.3)

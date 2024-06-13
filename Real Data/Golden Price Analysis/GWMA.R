48b1872830fad15deeb718f54126ccce49eb6c79
han-gnat <hedy890808@gmail.com>
2024-06-13 23:17:39 +0800

    Update GWMA.R

--- Real Data/Golden Price Analysis/GWMA.R
+++ Real Data/Golden Price Analysis/GWMA.R
@@ -1,6 +1,7 @@
 library(readr)
 library(ggplot2)
 
+# Load data
 Golden <- read_csv("price.csv")
 price <- Golden$price
 Date <- as.Date(Golden$Date)
@@ -9,29 +10,32 @@ x <- price
 mu <- mean(x[1:337])
 sigma <- sd(x)
 n <- length(x)
-q=0.8
-alpha=0.5
-L=2.605
 
-wt=vj=c()
+# set parameter
+q = 0.8
+alpha = 0.5
+L = 2.605
+
+# weight of GWMA
+wt = vj = c()
 for(i in 1:n){wt[i] = q^((i-1)^alpha)-q^(i^alpha)}
 for(j in 1:n){vj[j] = sum(wt[1:j]*wt[1:j])*(sigma^2)}
 
-gwma=ucl=lcl=c()
-
+# compute statistic of EWMA, UCL and LCL
+gwma = ucl = lcl = c()
 for(j in 1:n){
   zsum = sum(wt[1:j]*x[j:1]) + mu*(q^(j^alpha))
-  gwma[j]=zsum
-  ucl[j]=mu+L*sqrt(vj[j])
-  lcl[j]=mu-L*sqrt(vj[j])
+  gwma[j] = zsum
+  ucl[j] = mu+L*sqrt(vj[j])
+  lcl[j] = mu-L*sqrt(vj[j])
 }
 
+# Find the first index that exceeds the control limit
 exceed_indices <- which(gwma > ucl)[1]
-print(exceed_indices)
-
 
 gwma_df <- data.frame(Date = Date, GWMA = gwma, UCL = ucl, LCL = lcl)
 
+# plot result
 ggplot(gwma_df, aes(x = Date)) +
   geom_line(aes(y = GWMA), color = "black") +
   geom_line(aes(y = UCL), color = "red", linetype = "solid") +
@@ -42,6 +46,4 @@ ggplot(gwma_df, aes(x = Date)) +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_point(data = gwma_df[exceed_indices, ], aes(x = Date, y = GWMA), color = "blue", size = 1.5) +
   geom_text(data = gwma_df[exceed_indices, ], aes(x = Date, y = GWMA, label = format(Date, "%Y-%m")), vjust = -1,hjust=0.95, color = "blue",size=3.3)
-
-
 

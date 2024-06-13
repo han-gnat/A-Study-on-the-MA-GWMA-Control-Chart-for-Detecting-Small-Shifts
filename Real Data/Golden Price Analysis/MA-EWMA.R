e2c0df928144a8beac9e2fbd3403677d7deb9087
han-gnat <hedy890808@gmail.com>
2024-06-13 23:22:08 +0800

    Update MA-EWMA.R

--- Real Data/Golden Price Analysis/MA-EWMA.R
+++ Real Data/Golden Price Analysis/MA-EWMA.R
@@ -1,6 +1,7 @@
 library(readr)
 library(ggplot2)
 
+# Load data
 Golden <- read_csv("price.csv")
 price <- Golden$price
 Date <- as.Date(Golden$Date)
@@ -10,12 +11,14 @@ mu <- mean(x[1:337])
 sigma <- sd(x)
 n <- length(x)
 
-q=0.8
-alpha=1
-window=3
-L=4.2856
+# set parameter
+q = 0.8
+alpha = 1
+window = 3
+L = 4.2856
 
-wt=vj=c()
+# weight of MA-EMWA
+wt = vj = c()
 for(i in 1:n){ wt[i] = q^((i-1)^alpha)-q^(i^alpha) }
 for(j in 1:n){
   if(j >= window){
@@ -26,7 +29,8 @@ for(j in 1:n){
   }
 }
 
-ma_ewma=ucl=lcl=y=c()
+# compute statistic of MA-EWMA, UCL and LCL
+ma_ewma = ucl = lcl = y = c()
 for(j in 1:n){
   zsum = sum(wt[1:j]*x[j:1]) + mu*(q^(j^alpha))
   y = c(y, zsum)
@@ -36,23 +40,24 @@ for(j in 1:n){
   else{
     ma = mean(y[(j-window+1):j])
   }
-  ma_ewma[j]=ma
-  ucl[j]=mu+L*sqrt(vj[j])
-  lcl[j]=mu-L*sqrt(vj[j])
+  ma_ewma[j] = ma
+  ucl[j] = mu+L*sqrt(vj[j])
+  lcl[j] = mu-L*sqrt(vj[j])
 }
 
+# Find the first index that exceeds the control limit
 exceed_indices <- which(ma_ewma > ucl)[1]
-print(exceed_indices)
 
-ma_ewma_df <- data.frame(Date = Date, MA_EWMA=ma_ewma, UCL = ucl, LCL = lcl)
+ma_ewma_df <- data.frame(Date = Date, MA_EWMA = ma_ewma, UCL = ucl, LCL = lcl)
 
+# plot result
 ggplot(ma_ewma_df, aes(x = Date)) +
   geom_line(aes(y = MA_EWMA), color = "black") +
   geom_line(aes(y = UCL), color = "red", linetype = "solid") +
   geom_line(aes(y = LCL), color = "red", linetype = "solid") +
   labs(x = "Date", y = "MA-EWMA", title = "MA-EWMA Control Chart") +
   theme_bw() + 
-  theme(panel.grid=element_blank()) +
+  theme(panel.grid = element_blank()) +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_point(data = ma_ewma_df[exceed_indices, ], aes(x = Date, y = MA_EWMA), color = "blue", size = 1.5) +
-  geom_text(data = ma_ewma_df[exceed_indices, ], aes(x=Date,y=MA_EWMA,label=format(Date,"%Y-%m")),vjust=-1.2,hjust=0.8,color="blue",size=3.3)
+  geom_text(data = ma_ewma_df[exceed_indices, ], aes(x = Date,y = MA_EWMA, label = format(Date,"%Y-%m")), vjust = -1.2, hjust = 0.8, color = "blue", size = 3.3)
